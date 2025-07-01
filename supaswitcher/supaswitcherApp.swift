0dcb25b128cfaac68ded596bef328abdbc84f7e8
Kosta <burambek@gmail.com>
2025-07-01 19:51:40 +0300

    Initial Commit

---
+++ supaswitcher/supaswitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  supaswitcherApp.swift
+//  supaswitcher
+//
+//  Created by Kostiantyn Zhuha on 01.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct supaswitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
